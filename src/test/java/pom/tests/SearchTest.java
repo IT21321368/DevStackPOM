3bea3f628220e2f9e8ec7c173c56cd9b92ef052a
Vinali Samarasinghe <it21321368@my.sliit.lk>
2025-08-18 12:00:16 +0530

    add SearchTest

--- src/test/java/pom/tests/SearchTest.java
+++ src/test/java/pom/tests/SearchTest.java
@@ -3,33 +3,61 @@ package pom.tests;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pom.pages.ClothSearchPage;
 import pom.pages.MobileSearchPage;
+import pom.pages.SearchItemPage;
 import pom.utils.ThreadLocalWebDriver;
 
 public class SearchTest {
-    private MobileSearchPage mobileSearchPage;
-    private ClothSearchPage clothSearchPage;
+//    private MobileSearchPage mobileSearchPage;
+//    private ClothSearchPage clothSearchPage;
+    private SearchItemPage searchItemPage;
+
+    @DataProvider(name = "searchData")
+    public Object[][] searchItemData(){
+        return new Object[][]{
+                {"iphone","Cell Phones & Accessories"},
+                {"Men's T-shirt","Clothing, Shoes & Accessories"}
+        };
+    }
 
     @BeforeMethod
     public void setup(){
         WebDriver driver = ThreadLocalWebDriver.createWebDriver();
         driver.manage().window().maximize();
         driver.get("https://www.ebay.com/");
-        mobileSearchPage = new MobileSearchPage(driver);
-        clothSearchPage = new ClothSearchPage(driver);
+//        mobileSearchPage = new MobileSearchPage(driver);
+//        clothSearchPage = new ClothSearchPage(driver);
+
+        searchItemPage = new SearchItemPage(driver);
 
     }
 
-    @Test
-    public void searchForIphone(){
-        mobileSearchPage.searchMobile("iphone");
-    }
+//    @Test
+//    public void searchForIphone(){
+//        mobileSearchPage.searchMobile("iphone");
+//    }
+//
+//    @Test
+//    public void searchForMensTshirt(){
+//        clothSearchPage.searchClothing("Men's T-shirt");
+//    }
 
-    @Test
-    public void searchForMensTshirt(){
-        clothSearchPage.searchClothing("Men's T-shirt");
+//    @Test
+//    public void searchItemIphone(){
+//        searchItemPage.searchItem("iphone","Cell Phones & Accessories");
+//    }
+//
+//    @Test
+//    public void searchItemCloth(){
+//        searchItemPage.searchItem("Men's T-shirt","Clothing, Shoes & Accessories");
+//    }
+
+    @Test(dataProvider = "searchData")
+    public void searchForItem(String itemName, String categoryName){
+        searchItemPage.searchItem(itemName,categoryName);
     }
 
     @AfterMethod
